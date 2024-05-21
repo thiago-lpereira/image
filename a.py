c414f87e60c93c140645e683f105572e03d2ce04
thiago-lpereira <124629680+thiago-lpereira@users.noreply.github.com>
2024-05-21 08:27:06 -0300

    Update a.py

--- a.py
+++ a.py
@@ -1,1 +1,7 @@
-x = 1
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def devops():
+ return '<center><h1><font color=red>Germinare Tech, EU AMO DEVOPS</center>'
+if __name__ == '__main__':
+ app.run(debug=True, host='0.0.0.0')
