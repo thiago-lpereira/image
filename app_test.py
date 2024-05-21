b45fab6d3af39bb2aeab2fdd604e7909ffe96c1a
thiago-lpereira <124629680+thiago-lpereira@users.noreply.github.com>
2024-05-21 08:52:34 -0300

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -2,7 +2,7 @@ import unittest
 from app import app
 class FlaskAppTests(unittest.TestCase):
  def setUp(self):
- self.client = app.test_client()
+  self.client = app.test_client()
 def test_devops(self):
  response = self.client.get('/')
  self.assertEqual(response.status_code, 200)
