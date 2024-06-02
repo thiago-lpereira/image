0da2ee720e67f864244862465e389e80e51a6ad0
thiago-lpereira <124629680+thiago-lpereira@users.noreply.github.com>
2024-06-02 18:30:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def devops():
     return '<center><h1><font color=red>Germinare Tech, EU AMO DEVOPS</center>'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=5000)
